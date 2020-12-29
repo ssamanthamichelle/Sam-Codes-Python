6a0fde9729cad58a269bd047275244f290dba911
ssamanthamichelle <58668677+ssamanthamichelle@users.noreply.github.com>
2020-12-29 15:21:14 -0500

    Add files via upload

---
+++ python01-code.py
@@ -0,0 +1,59 @@
+#Sam Codes
+
+"""
+
+	HEY GUYS, its me SAM
+
+	this is the code from Python 01 on my youtube channel... Sam Codes
+
+	feel free to download and run this!
+
+	byeeee
+
+	;)
+
+"""
+
+#Hello World!
+print("Hello World!")
+
+
+#####################################
+#Hello World! but as a function ~
+
+#hello_world
+def hello_world():
+	print("Hello World!")
+
+
+#calling our function
+hello_world()
+
+#one more time ;)
+hello_world()
+
+################################
+#a function like hello world
+#hello
+
+def hello(x):
+	print("Hello" + " " + x)
+
+
+#calling our function
+
+#using "Samantha" as an argument
+hello("Samantha")
+
+#using "Bryanna" as an argument
+hello("Bryanna")
+
+#and another argument :o
+hello("to everyone watching this video!")
+
+#try calling hello() with your own arguments!!
+
+#...
+
+
+
