07f8e910e3dc3eb0f36b61ba12eabdfd96db04f1
ssamanthamichelle <58668677+ssamanthamichelle@users.noreply.github.com>
2021-01-13 17:52:06 -0500

    Add files via upload

---
+++ python03-code.py
@@ -0,0 +1,43 @@
+"""
+	HEY GUYS, it's me SAM
+
+	this is the code from my video python 03 on youtube...
+
+	feel free to download and run this!
+
+	byeee ;)
+
+"""
+
+#formatting
+print("------------------------------------------------")
+print("Using the input function with an argument")
+print("\n")
+
+def greeting():
+
+	user_name = input("Enter your name: ")
+	print("Hello " + user_name + "!")
+
+
+greeting()
+
+
+#formatting
+print("\n\n")
+print("------------------------------------------------")
+print("Using the input function without an argument")
+print("\n")
+
+
+def other_greeting():
+
+	print("Enter your name:")
+	user_name = input()
+	print("Hello " + user_name + "!")
+
+
+other_greeting()
+
+print("\n")
+
